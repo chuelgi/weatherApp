2c96355b939abb76a6b1151ed06b15c1dcb920cf
AHearn <ahearn1@emich.edu>
2024-03-09 20:36:22 -0500

    update index page syling add function to switch between day/night theme

--- static/script.js
+++ static/script.js
@@ -2,13 +2,18 @@ function setTheme() {
 	var now = new Date();
 	var hour = now.getHours();
 	var body = document.body;
+	var mainIframe = document.getElementById('mainiframe');
 
 	if (hour >= 6 && hour < 18) {
 		body.classList.remove('night');
 		body.classList.add('day');
+		mainIframe.src =
+			'https://my.spline.design/earthday-9d45310cde1d5aa5e7eafd5319e21a37/';
 	} else {
 		body.classList.remove('day');
 		body.classList.add('night');
+		mainIframe.src =
+			'https://my.spline.design/untitled-314291d07ca525747f5d7034eda789d8/';
 	}
 }
 
